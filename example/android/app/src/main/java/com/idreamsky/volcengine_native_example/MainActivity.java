6ee2e72eaf08f9f5ae546f5cce80cb085769c4d1
qubian <qubian@idreamsky.com>
2022-11-28 15:43:20 +0800

    first code

---
+++ example/android/app/src/main/java/com/idreamsky/volcengine_native_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.idreamsky.volcengine_native_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ ios/Classes/VolcengineNativePlugin.m
@@ -0,0 +1,20 @@
+#import "VolcengineNativePlugin.h"
+
+@implementation VolcengineNativePlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"volcengine_native"
+            binaryMessenger:[registrar messenger]];
+  VolcengineNativePlugin* instance = [[VolcengineNativePlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
